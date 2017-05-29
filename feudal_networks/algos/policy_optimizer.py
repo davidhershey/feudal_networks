fbe6f9d4447d57c3d5b1bc4efe1ebc993645f037
wulfebw <wulfebw@stanford.edu>
2017-05-29 16:58:49 -0700

    adding optimizer and lstm policy

--- feudal_networks/algos/policy_optimizer.py
+++ feudal_networks/algos/policy_optimizer.py
@@ -1,20 +1,203 @@
+"""
+Much of the code in this file was originally developed as part of the 
+universe starter agent: https://github.com/openai/universe-starter-agent
+"""
+from collections import namedtuple
+import numpy as np
+import scipy.signal
+import tensorflow as tf
+import threading
 
+def discount(x, gamma):
+    return scipy.signal.lfilter([1], [1, -gamma], x[::-1], axis=0)[::-1]
+
+def process_rollout(rollout, gamma, lambda_=1.0):
+    """
+    given a rollout, compute its returns and the advantage
+    """
+    batch_si = np.asarray(rollout.states)
+    batch_a = np.asarray(rollout.actions)
+    rewards = np.asarray(rollout.rewards)
+    vpred_t = np.asarray(rollout.values + [rollout.r])
+
+    rewards_plus_v = np.asarray(rollout.rewards + [rollout.r])
+    batch_r = discount(rewards_plus_v, gamma)[:-1]
+    delta_t = rewards + gamma * vpred_t[1:] - vpred_t[:-1]
+    # this formula for the advantage comes "Generalized Advantage Estimation":
+    # https://arxiv.org/abs/1506.02438
+    batch_adv = discount(delta_t, gamma * lambda_)
+
+    features = rollout.features[0]
+    return Batch(batch_si, batch_a, batch_adv, batch_r, rollout.terminal, features)
+
+Batch = namedtuple("Batch", ["obs", "a", "returns", "terminal", "s", "g", "features"])
+
+class PartialRollout(object):
+    """
+    a piece of a complete rollout.  We run our agent, and process its experience
+    once it has processed enough steps.
+    """
+    def __init__(self):
+        self.states = []
+        self.actions = []
+        self.rewards = []
+        self.values = []
+        self.r = 0.0
+        self.terminal = False
+        self.features = []
+
+    def add(self, state, action, reward, value, terminal, features):
+        self.states += [state]
+        self.actions += [action]
+        self.rewards += [reward]
+        self.values += [value]
+        self.terminal = terminal
+        self.features += [features]
+
+    def extend(self, other):
+        assert not self.terminal
+        self.states.extend(other.states)
+        self.actions.extend(other.actions)
+        self.rewards.extend(other.rewards)
+        self.values.extend(other.values)
+        self.r = other.r
+        self.terminal = other.terminal
+        self.features.extend(other.features)
+
+class RunnerThread(threading.Thread):
+    """
+    One of the key distinctions between a normal environment and a universe environment
+    is that a universe environment is _real time_.  This means that there should be a thread
+    that would constantly interact with the environment and tell it what to do.  This thread is here.
+    """
+    def __init__(self, env, policy, num_local_steps, visualise):
+        threading.Thread.__init__(self)
+        self.queue = queue.Queue(5)
+        self.num_local_steps = num_local_steps
+        self.env = env
+        self.last_features = None
+        self.policy = policy
+        self.daemon = True
+        self.sess = None
+        self.summary_writer = None
+        self.visualise = visualise
+
+    def start_runner(self, sess, summary_writer):
+        self.sess = sess
+        self.summary_writer = summary_writer
+        self.start()
+
+    def run(self):
+        with self.sess.as_default():
+            self._run()
+
+    def _run(self):
+        rollout_provider = env_runner(self.env, self.policy, self.num_local_steps, self.summary_writer, self.visualise)
+        while True:
+            # the timeout variable exists because apparently, if one worker dies, the other workers
+            # won't die with it, unless the timeout is set to some large number.  This is an empirical
+            # observation.
+
+            self.queue.put(next(rollout_provider), timeout=600.0)
+
+def env_runner(env, policy, num_local_steps, summary_writer):
+    """
+    The logic of the thread runner.  In brief, it constantly keeps on running
+    the policy, and as long as the rollout exceeds a certain length, the thread
+    runner appends the policy to the queue.
+    """
+    last_state = env.reset()
+    last_features = policy.get_initial_features()
+    length = 0
+    rewards = 0
+
+    while True:
+        terminal_end = False
+        rollout = PartialRollout()
+
+        for _ in range(num_local_steps):
+            fetched = policy.act(last_state, *last_features)
+            action, value_, features = fetched[0], fetched[1], fetched[2:]
+            action_to_take = action.argmax()
+            state, reward, terminal, info = env.step(action_to_take)
+
+            # collect the experience
+            rollout.add(last_state, action, reward, value_, terminal, last_features)
+            length += 1
+            rewards += reward
+
+            last_state = state
+            last_features = features
+
+            if info:
+                summary = tf.Summary()
+                for k, v in info.items():
+                    summary.value.add(tag=k, simple_value=float(v))
+                summary_writer.add_summary(summary, policy.global_step.eval())
+                summary_writer.flush()
+
+            timestep_limit = env.spec.tags.get('wrapper_config.TimeLimit.max_episode_steps')
+            if terminal or length >= timestep_limit:
+                terminal_end = True
+                if length >= timestep_limit or not env.metadata.get('semantics.autoreset'):
+                    last_state = env.reset()
+                last_features = policy.get_initial_features()
+                print("Episode finished. Sum of rewards: %f. Length: %d" % (rewards, length))
+                length = 0
+                rewards = 0
+                break
+
+        if not terminal_end:
+            rollout.r = policy.value(last_state, *last_features)
+
+        # once we have enough experience, yield it, and have the ThreadRunner place it on a queue
+        yield rollout
 
 class PolicyOptimizer(object):
-    def __init__(self, env, global_policy, policy):
-        """
-        - creates runner
-        - builds grads
-        - builds train op
-        - builds sync op
-        """
-        pass
+    def __init__(self, env, global_policy,global_step, policy, config):
+        self.env = env
+        self.task = task
+        self.global_policy = global_policy
+        self.global_step = global_step
+        self.policy = pi = policy
+        self.config = config
+
+        # build runner thread for collecting rollouts
+        self.runner = RunnerThread(env, self.policy, config.update_steps)
+
+        # formulate gradients
+        grads = tf.gradients(pi.loss, pi.var_list)
+        grads, _ = tf.clip_by_global_norm(grads, config.max_global_grad_norm)
+
+        # build sync
+        # copy weights from the parameter server to the local model
+        self.sync = tf.group(*[v1.assign(v2) 
+            for v1, v2 in zip(pi.var_list, self.global_policy.var_list)])
+        grads_and_vars = list(zip(grads, self.global_policy.var_list))
+        # TODO: avoid accessing pi.x here somehow
+        inc_step = self.global_step.assign_add(tf.shape(pi.x)[0])
+
+        # build train op
+        opt = tf.train.AdamOptimizer(config.lr)
+        self.train_op = tf.group(opt.apply_gradients(grads_and_vars), inc_step)
+        self.summary_writer = None
+        self.local_steps = 0
 
     def start(self, sess, summary_writer):
-        pass
+        self.runner.start_runner(sess, summary_writer)
+        self.summary_writer = summary_writer
 
     def pull_batch_from_queue(self):
-        pass
+        """
+        self explanatory:  take a rollout from the queue of the thread runner.
+        """
+        rollout = self.runner.queue.get(timeout=600.0)
+        while not rollout.terminal:
+            try:
+                rollout.extend(self.runner.queue.get_nowait())
+            except queue.Empty:
+                break
+        return rollout
 
     def train(self, sess):
         """
@@ -23,4 +206,14 @@ class PolicyOptimizer(object):
         converts it to a batch, and passes that batch and the train op to the
         policy to perform an update. 
         """
-        pass
+        # copy weights from shared to local 
+        # this should be run first so that the updates are for the most 
+        # recent global weights
+        sess.run(self.sync)  
+        rollout = self.pull_batch_from_queue()
+        batch = process_rollout(rollout, gamma=self.config.gamma, 
+            lambda_=self.config.lambda_)
+        compute_summary = self.task == 0 and self.local_steps % 11 == 0
+        self.policy.update(sess, self.train_op, batch, self.summary_writer,
+            compute_summary)
+        self.local_steps += 1
---
+++ feudal_networks/policies/lstm_policy.py
@@ -0,0 +1,118 @@
+
+import gym
+import numpy as np
+import tensorflow as tf
+
+from feudal_networks.models.models import (linear, conv2d, build_lstm,
+    normalized_columns_initializer)
+import feudal_networks.policies.policy_utils as policy_utils
+
+class LSTMPolicy(object):
+    def __init__(self, obs_space, act_space, config):
+        self.obs_space = obs_space
+        self.act_space = act_space
+        self.config = config
+        self.local_steps = 0
+        # build placeholders
+        self.x = x = tf.placeholder(tf.float32, 
+                                    [None] + list(obs_space), 
+                                    name='state')
+        self.adv = tf.placeholder(tf.float32, 
+                                    [None], 
+                                    name="adv")
+        self.ac = tf.placeholder(tf.float32, 
+                                    [None, act_space], 
+                                    name="ac")
+        self.r = tf.placeholder(tf.float32, 
+                                    [None], 
+                                    name="r")
+
+        # build perception
+        for i in range(config.n_percept_hidden_layer):
+            x = tf.nn.elu(conv2d(x, config.n_percept_filters, 
+                "l{}".format(i + 1), [3, 3], [2, 2]))
+
+        # introduce a "fake" batch dimension of 1 after flatten so that we 
+        # can do LSTM over time dim
+        x = tf.expand_dims(policy_utils.flatten(x), [0])
+        x, self.state_init, self.state_in, self.state_out = build_lstm(
+            x, config.size, 'lstm', tf.shape(self.x)[:1])
+
+        # add hidden layer to value output so that less of a burden is placed 
+        # on the lstm to output values for both the policy and value function
+        vfhid = tf.nn.elu(linear(x, config.size, "value_hidden", 
+            normalized_columns_initializer(0.01)))
+        self.vf = tf.reshape(linear(vfhid, 1, "value", 
+            normalized_columns_initializer(1.0)), [-1])
+
+        # retrieve logits, sampling op
+        self.logits = linear(x, act_space, "action", 
+            normalized_columns_initializer(0.01))
+        self.sample = policy_utils.categorical_sample(
+            self.logits, act_space)[0, :]
+        self.var_list = tf.get_collection(
+            tf.GraphKeys.TRAINABLE_VARIABLES, tf.get_variable_scope().name)
+
+        # build loss
+        log_prob_tf = tf.nn.log_softmax(self.logits)
+        prob_tf = tf.nn.softmax(self.logits)
+        pi_loss = - tf.reduce_sum(
+                    tf.reduce_sum(log_prob_tf * self.ac, [1]) * self.adv)
+        entropy = - tf.reduce_sum(prob_tf * log_prob_tf)
+        vf_loss = 0.5 * tf.reduce_sum(tf.square(self.vf - self.r))
+        beta = tf.train.polynomial_decay(config.beta_start, self.global_step, 
+                end_learning_rate=config.beta_end,
+                decay_steps=config.decay_steps, 
+                power=1)
+        self.loss = pi_loss + 0.5 * vf_loss - entropy * beta
+
+        # summaries
+        bs = tf.to_float(tf.shape(self.x)[0])
+        tf.summary.scalar("model/policy_loss", pi_loss / bs)
+        tf.summary.scalar("model/value_mean", tf.reduce_mean(self.vf))
+        tf.summary.scalar("model/value_loss", vf_loss / bs)
+        tf.summary.scalar("model/value_loss_scaled", vf_loss / bs * .5)
+        tf.summary.scalar("model/entropy", entropy / bs)
+        tf.summary.scalar("model/entropy_loss_scaleed", -entropy / bs * beta)
+        tf.summary.scalar("model/grad_global_norm", tf.global_norm(grads))
+        tf.summary.scalar("model/var_global_norm", tf.global_norm(self.var_list))
+        tf.summary.scalar("model/beta", beta)
+        tf.summary.image("model/state", self.x)
+        self.summary_op = tf.summary.merge_all()
+
+    def get_initial_features(self):
+        return self.state_init
+
+    def act(self, ob, c, h):
+        sess = tf.get_default_session()
+        return sess.run([self.sample, self.vf] + self.state_out,
+                        {self.x: [ob], self.state_in[0]: c, self.state_in[1]: h})
+
+    def value(self, ob, c, h):
+        sess = tf.get_default_session()
+        return sess.run(self.vf, {self.x: [ob], self.state_in[0]: c, self.state_in[1]: h})[0]
+
+    def update(self, sess, train_op, batch, global_step, summary_writer):
+        self.local_steps += 1
+        compute_summary = self.local_steps % self.config.summary_steps == 0
+        if compute_summary:
+            fetches = [self.summary_op, train_op, global_step]
+        else:
+            fetches = [train_op, global_step]
+
+        feed_dict = {
+            self.x: batch.si,
+            self.ac: batch.a,
+            self.adv: batch.adv,
+            self.r: batch.r,
+            self.state_in[0]: batch.features[0],
+            self.state_in[1]: batch.features[1],
+        }
+
+        fetched = sess.run(fetches, feed_dict=feed_dict)
+
+        if compute_summary:
+            summary_writer.add_summary(
+                tf.Summary.FromString(fetched[0]), fetched[-1])
+            summary_writer.flush()
+        
