1ba55e2a0903005fe428ed956ea2f2399541755d
wulfebw <wulfebw@stanford.edu>
2017-05-29 21:49:18 -0700

    adding feudal batch processor and tests

---
+++ feudal_networks/policies/feudal_batch_processor.py
@@ -0,0 +1,126 @@
+
+import numpy as np
+
+def cosine_similarity(u, v):
+    return np.dot(np.transpose(u),v) / (np.linalg.norm(u) * np.linalg.norm(v))
+
+class FeudalBatch(object):
+    def __init__(self):
+        self.obs = []
+        self.a = []
+        self.returns = []
+        self.s_diff = []
+        self.ri = []
+        self.gsum = []
+        self.features = []
+
+    def add(self, obs, a, returns, s_diff, ri, gsum, features):
+        self.obs += [obs]
+        self.a += [a]
+        self.returns += [returns]
+        self.s_diff += [s_diff]
+        self.ri += [ri]
+        self.gsum += [gsum]
+        self.features += [features]
+
+class FeudalBatchProcessor(object):
+    """
+    This class adapts the batch of PolicyOptimizer to a batch useable by
+    the FeudalPolicy.
+    """
+    def __init__(self, c):
+        self.c = c
+        self.last_terminal = True
+
+    def _extend(self, batch):
+        # if the last batch was a terminal one, then preprend with the first 
+        # s and g repeated c times
+        if self.last_terminal:
+            self.last_terminal = False
+            self.s = [batch.s[0] for _ in range(self.c)]
+            self.g = [batch.g[0] for _ in range(self.c)]
+            # prepend with dummy values so indexing is the same
+            self.obs = [None for _ in range(self.c)]
+            self.a = [None for _ in range(self.c)]
+            self.returns = [None for _ in range(self.c)]
+            self.features = [None for _ in range(self.c)]
+
+        # extend with the actual values
+        self.obs.extend(batch.obs)
+        self.a.extend(batch.a)
+        self.returns.extend(batch.returns)
+        self.s.extend(batch.s)
+        self.g.extend(batch.g)
+        self.features.extend(batch.features)
+
+        # if this is a terminal batch, then append the final s and g c times
+        # note that both this and the above case can occur at the same time
+        if batch.terminal:
+            self.s.extend([batch.s[-1] for _ in range(self.c)])
+            self.g.extend([batch.g[-1] for _ in range(self.c)])
+
+    def process_batch(self, batch):
+        """
+        Converts a normal batch into one used by the FeudalPolicy update.
+
+        FeudalPolicy requires a batch of the form:
+
+        c previous timesteps - batch size timesteps - c future timesteps
+
+        This class handles the tracking the leading and following timesteps over
+        time. Additionally, it also computes values across timesteps from the
+        batch to provide to FeudalPolicy.
+        """
+        # extend with current batch
+        self._extend(batch)
+
+        # unpack and compute bounds 
+        length = len(self.obs)
+        c = self.c
+
+        # normally we cannot compute samples for the last c elements, but 
+        # in the terminal case, we halluciante values where necessary
+        end = length if batch.terminal else length - c
+
+        # collect samples to return in a FeudalBatch
+        feudal_batch = FeudalBatch()
+        for t in range(c, end):
+
+            # state difference 
+            s_diff = self.s[t + c] - self.s[t]
+
+            # intrinsic reward
+            ri = 0
+            # note that this for loop considers s and g values
+            # 1 timestep to c timesteps (inclusively) ago
+            for i in range(1, c + 1):
+                ri_s_diff = self.s[t] - self.s[t - i]
+                if np.linalg.norm(ri_s_diff) != 0:
+                    ri += cosine_similarity(ri_s_diff, self.g[t - i])
+            ri /= c
+
+            # sum of g values used to derive w, input to the linear transform
+            gsum = np.zeros_like(self.g[t - c])
+            for i in range(t - c, t + 1):
+                gsum += self.g[i]
+
+            # add to the batch
+            feudal_batch.add(self.obs[t], self.a[t], self.returns[t], s_diff,
+                ri, gsum, self.features[t])
+
+        # in the terminal case, set reset flag
+        if batch.terminal:
+            self.last_terminal = True
+        # in the general case, forget all but the last 2 * c elements
+        # reason being that the first c of those we have already computed 
+        # a batch for, and the second c need those first c 
+        else:
+            twoc = 2 * self.c
+            self.obs = self.obs[-twoc:]
+            self.a = self.a[-twoc:]
+            self.returns = self.returns[-twoc:]
+            self.s = self.s[-twoc:]
+            self.g = self.g[-twoc:]
+            self.features = self.features[-twoc:]
+       
+        return feudal_batch
