bfc04690a925015892212dd0a6cae996b64ba64b
wulfebw <wulfebw@stanford.edu>
2017-05-31 01:58:51 -0700

    adding maze environment

--- feudal_networks/envs/__init__.py
+++ feudal_networks/envs/__init__.py
@@ -5,3 +5,13 @@ register(
     entry_point='feudal_networks.envs.debug_envs:OneRoundDeterministicRewardBoxObsEnv',
     max_episode_steps=1,
 )
+
+register(
+    id='VisionMaze-v0',
+    entry_point='feudal_networks.envs.vision_maze:VisionMazeEnv',
+    max_episode_steps=200,
+    kwargs = {
+        'room_length': 3,
+        'num_rooms_per_side': 2
+    }
+)
