Fixed manager loss sign

feudal_networks/algos/feudal_policy_optimizer.py
@@ -109,6 +109,7 @@ class RunnerThread(threading.Thread):
 
             self.queue.put(next(rollout_provider), timeout=600.0)
 
+from collections import deque
 def env_runner(env, policy, num_local_steps, summary_writer,visualise):
     """
     The logic of the thread runner.  In brief, it constantly keeps on running
@@ -120,7 +121,7 @@ def env_runner(env, policy, num_local_steps, summary_writer,visualise):
     # print last_c_g
     length = 0
     rewards = 0
-
+    reward_list = deque(maxlen=30)
     while True:
         terminal_end = False
         rollout = PartialRollout()
@@ -160,7 +161,9 @@ def env_runner(env, policy, num_local_steps, summary_writer,visualise):
                 if length >= timestep_limit or not env.metadata.get('semantics.autoreset'):
                     last_state = env.reset()
                 last_c_g,last_features = policy.get_initial_features()
-                print("Episode finished. Sum of rewards: %f. Length: %d" % (rewards, length))
+                reward_list.append(rewards)
+                mean_reward = np.mean(list(reward_list))
+                print("Episode finished. Sum of rewards: %f. Length: %d.  Mean Reward: %f" % (rewards, length,mean_reward))
                 length = 0
                 rewards = 0
                 break

feudal_networks/policies/feudal_policy.py
@@ -69,22 +69,22 @@ class FeudalPolicy(policy.Policy):
 
 
     def _build_perception(self):
-        conv1 = tf.layers.conv2d(inputs=self.obs,
-                                filters=16,
-                                kernel_size=[8, 8],
-                                activation=tf.nn.elu,
-                                strides=4)
-        conv2 = tf.layers.conv2d(inputs=conv1,
-                                filters=32,
-                                kernel_size=[4,4],
-                                activation=tf.nn.elu,
-                                strides=2)
-        flattened_filters = policy_utils.flatten(conv2)
-        # x = self.obs
-        # for i in range(4):
-        #     x = tf.nn.elu(conv2d(x, 32,
-        #         "l{}".format(i + 1), [3, 3], [2, 2]))
-        # flattened_filters = policy_utils.flatten(x)
+        # conv1 = tf.layers.conv2d(inputs=self.obs,
+        #                         filters=16,
+        #                         kernel_size=[8, 8],
+        #                         activation=tf.nn.elu,
+        #                         strides=4)
+        # conv2 = tf.layers.conv2d(inputs=conv1,
+        #                         filters=32,
+        #                         kernel_size=[4,4],
+        #                         activation=tf.nn.elu,
+        #                         strides=2)
+        # flattened_filters = policy_utils.flatten(conv2)
+        x = self.obs
+        for i in range(4):
+            x = tf.nn.elu(conv2d(x, 32,
+                "l{}".format(i + 1), [3, 3], [2, 2]))
+        flattened_filters = policy_utils.flatten(x)
         self.z = tf.layers.dense(inputs=flattened_filters,\
                                 units=256,\
                                 activation=tf.nn.elu)
@@ -156,10 +156,10 @@ class FeudalPolicy(policy.Policy):
     def _build_loss(self):
         cutoff_vf_manager = tf.reshape(tf.stop_gradient(self.manager_vf),[-1])
         dot = tf.reduce_sum(tf.multiply(self.s_diff,self.g ),axis=1)
-        gcut = tf.stop_gradient(self.g)
+        gcut = self.g
         mag = tf.norm(self.s_diff,axis=1)*tf.norm(gcut,axis=1)+.0001
         dcos = dot/mag
-        manager_loss = -tf.reduce_sum((self.r-cutoff_vf_manager)*dcos)
+        manager_loss = tf.reduce_sum((self.r-cutoff_vf_manager)*dcos)
 
         cutoff_vf_worker = tf.reshape(tf.stop_gradient(self.worker_vf),[-1])
         log_p = tf.reduce_sum(self.log_pi*self.ac,[1])