Fixed calculation of prev_g, made it get directly passed into batches

feudal_networks/algos/feudal_policy_optimizer.py
@@ -19,23 +19,24 @@ def process_rollout(rollout, manager_gamma, worker_gamma, lambda_=1.0):
     """
 given a rollout, compute its returns and the advantage
 """
-    batch_si = np.asarray(rollout.states)
-    batch_a = np.asarray(rollout.actions)
+    batch_si = rollout.states
+    batch_a = rollout.actions
 
-    rewards = np.asarray(rollout.rewards)
-    manager_rewards_plus_v = np.asarray(rollout.rewards + [rollout.manager_r])
-    worker_rewards_plus_v = np.asarray(rollout.rewards + [rollout.worker_r])
+    rewards = rollout.rewards
+    manager_rewards_plus_v = rollout.rewards + [rollout.manager_r]
+    worker_rewards_plus_v = rollout.rewards + [rollout.worker_r]
     batch_manager_r = discount(manager_rewards_plus_v, manager_gamma)[:-1]
     batch_worker_r = discount(worker_rewards_plus_v, worker_gamma)[:-1]
 
-    batch_s = np.asarray(rollout.ss)
-    batch_g = np.asarray(rollout.gs)
+    batch_s = rollout.ss
+    batch_g = rollout.gs
+    batch_g_prev = rollout.g_prev
     features = rollout.features
-    return Batch(batch_si, batch_a, batch_manager_r, batch_worker_r, 
-        rollout.terminal, batch_s, batch_g, features)
+    return Batch(batch_si, batch_a, batch_manager_r, batch_worker_r,
+        rollout.terminal, batch_s, batch_g,batch_g_prev, features)
 
-Batch = namedtuple("Batch", ["obs", "a", "manager_returns", "worker_returns", 
-    "terminal", "s", "g", "features"])
+Batch = namedtuple("Batch", ["obs", "a", "manager_returns", "worker_returns",
+    "terminal", "s", "g","g_prev", "features"])
 
 class PartialRollout(object):
     """
@@ -49,12 +50,13 @@ class PartialRollout(object):
         self.values = []
         self.ss = []
         self.gs = []
+        self.g_prev = []
         self.features = []
         self.manager_r = 0.0
         self.worker_r = 0.0
         self.terminal = False
 
-    def add(self, state, action, reward, value,g,s, terminal, features):
+    def add(self, state, action, reward, value,g,s,g_prev, terminal, features):
         self.states += [state]
         self.actions += [action]
         self.rewards += [reward]
@@ -62,6 +64,7 @@ class PartialRollout(object):
         self.terminal = terminal
         self.features += [features]
         self.gs += [g]
+        self.g_prev += [g_prev]
         self.ss += [s]
 
     def extend(self, other):
@@ -71,6 +74,7 @@ class PartialRollout(object):
         self.rewards.extend(other.rewards)
         self.values.extend(other.values)
         self.gs.extend(other.gs)
+        self.g_prev.extend(other.g_prev)
         self.ss.extend(other.ss)
         self.manager_r = other.manager_r
         self.worker_r = other.worker_r
@@ -131,7 +135,7 @@ def env_runner(env, policy, num_local_steps, summary_writer,visualise):
         rollout = PartialRollout()
 
         for local_step_iter in range(num_local_steps):
-            # print last_c_g.shape
+            # print last_c_g
             fetched = policy.act(last_state,last_c_g, *last_features)
             action, value_, g, s, last_c_g, features = fetched[0], fetched[1], \
                                                     fetched[2], fetched[3], \
@@ -141,7 +145,7 @@ def env_runner(env, policy, num_local_steps, summary_writer,visualise):
             state, reward, terminal, info = env.step(action_to_take)
 
             # collect the experience
-            rollout.add(last_state, action, reward, value_, g, s, terminal, last_features)
+            rollout.add(last_state, action, reward, value_, g, s,last_c_g, terminal, last_features)
             length += 1
             rewards += reward
 
@@ -164,6 +168,7 @@ def env_runner(env, policy, num_local_steps, summary_writer,visualise):
                 reward_list.append(rewards)
                 mean_reward = np.mean(list(reward_list))
                 print("Episode finished. Sum of rewards: %f. Length: %d.  Mean Reward: %f" % (rewards, length,mean_reward))
+                # sleep(.2)
                 length = 0
                 rewards = 0
                 break
@@ -192,12 +197,12 @@ class FeudalPolicyOptimizer(object):
 
         with tf.device(global_device):
             with tf.variable_scope("global"):
-                self.global_step = tf.get_variable("global_step", [], tf.int32, 
+                self.global_step = tf.get_variable("global_step", [], tf.int32,
                     initializer=tf.constant_initializer(0, dtype=tf.int32),
                     trainable=False)
                 self.network = FeudalPolicy(
-                    env.observation_space.shape, 
-                    env.action_space.n, 
+                    env.observation_space.shape,
+                    env.action_space.n,
                     self.global_step,
                     config
                 )
@@ -205,7 +210,7 @@ class FeudalPolicyOptimizer(object):
         with tf.device(worker_device):
             with tf.variable_scope("local"):
                 self.local_network = pi = FeudalPolicy(
-                    env.observation_space.shape, 
+                    env.observation_space.shape,
                     env.action_space.n,
                     self.global_step,
                     config
@@ -279,7 +284,7 @@ class FeudalPolicyOptimizer(object):
         # recent global weights
         sess.run(self.sync)
         rollout = self.pull_batch_from_queue()
-        batch = process_rollout(rollout, 
+        batch = process_rollout(rollout,
             manager_gamma=self.config.manager_discount,
             worker_gamma=self.config.worker_discount)
         batch = self.policy.update_batch(batch)
@@ -287,7 +292,7 @@ class FeudalPolicyOptimizer(object):
         should_compute_summary = self.task == 0 and self.local_steps % 11 == 0
 
         if should_compute_summary:
-            fetches = [self.summary_op, self.policy.summary_op, self.train_op, 
+            fetches = [self.summary_op, self.policy.summary_op, self.train_op,
                 self.global_step]
         else:
             fetches = [self.train_op, self.global_step]
@@ -308,8 +313,8 @@ class FeudalPolicyOptimizer(object):
             self.policy.s_diff: batch.s_diff,
             self.network.s_diff: batch.s_diff,
 
-            self.policy.prev_g: batch.gsum,
-            self.network.prev_g: batch.gsum,
+            self.policy.prev_g: batch.g_prev,
+            self.network.prev_g: batch.g_prev,
 
             self.policy.ri: batch.ri,
             self.network.ri: batch.ri

feudal_networks/policies/feudal_batch_processor.py
@@ -4,7 +4,7 @@ from collections import namedtuple
 from scipy.stats import multivariate_normal
 
 def cosine_similarity(u, v, eps=1e-8):
-    return (np.dot(np.squeeze(u), np.squeeze(v)) 
+    return (np.dot(np.squeeze(u), np.squeeze(v))
             / (np.linalg.norm(u) * np.linalg.norm(v) + eps))
 
 def normalized_gaussian_pdf(x, mean):
@@ -12,32 +12,43 @@ def normalized_gaussian_pdf(x, mean):
     return np.exp(-np.dot(diff.T, diff))
 
 Batch = namedtuple("Batch", ["obs", "a", "manager_returns", "worker_returns",
-    "s_diff", "ri", "gsum", "features"])
+    "s_diff", "ri", "gsum","g_prev", "features"])
 
 class FeudalBatch(object):
     def __init__(self):
         self.obs = []
+        self.g_prev = []
         self.a = []
         self.manager_returns = []
         self.worker_returns = []
         self.s_diff = []
         self.ri = []
         self.gsum = []
-        self.features = None
+        self.features = []
 
-    def add(self, obs, a, manager_returns, worker_returns, s_diff, ri, gsum, features):
+    def add(self, obs, a, manager_returns, worker_returns, s_diff, ri, gsum,g_prev, features):
         self.obs += [obs]
+        self.g_prev += [g_prev]
         self.a += [a]
         self.manager_returns += [manager_returns]
         self.worker_returns += [worker_returns]
         self.s_diff += [s_diff]
         self.ri += [ri]
         self.gsum += [gsum]
-        if self.features is None:
-            self.features = features
+        self.features +=[features]
+        # if self.features is None:
+            # self.features = features
 
     def get_batch(self):
         batch_obs = np.asarray(self.obs)
+        batch_g_prev = np.squeeze(np.asarray(self.g_prev))
+        sums = []
+        for i in range(len(batch_g_prev)):
+            sums.append(np.sum(batch_g_prev[i],axis=1))
+            # np.sum(batch_g_prev[i],axis=1)
+        # for g in self.g_prev:
+        #     print g.shape
+        #  self.g_prev
         batch_a = np.asarray(self.a)
         batch_manager_returns = np.asarray(self.manager_returns)
         batch_worker_returns = np.asarray(self.worker_returns)
@@ -45,9 +56,9 @@ class FeudalBatch(object):
         if len(batch_sd.shape) >= 2 and batch_sd.shape[1] == 1:
             batch_sd = np.squeeze(batch_sd, axis=1)
         batch_ri = np.asarray(self.ri)
-        batch_gs = np.asarray(self.gsum)
-        return Batch(batch_obs, batch_a, batch_manager_returns, 
-            batch_worker_returns, batch_sd, batch_ri, batch_gs, self.features)
+        batch_gs = np.asarray(sums)
+        return Batch(batch_obs, batch_a, batch_manager_returns,
+            batch_worker_returns, batch_sd, batch_ri, batch_gs,batch_g_prev, self.features[0])
 
 class FeudalBatchProcessor(object):
     """
@@ -61,7 +72,7 @@ class FeudalBatchProcessor(object):
         if similarity_metric == 'cosine':
             self.similarity = cosine_similarity
         elif similarity_metric == 'gaussian':
-            self.similarity = normalized_gaussian_pdf 
+            self.similarity = normalized_gaussian_pdf
         else:
             raise ValueError('invalid similarity metric: {}'.format(
                 similarity_metric))
@@ -78,6 +89,7 @@ class FeudalBatchProcessor(object):
 
         # prepend with dummy values so indexing is the same
         self.obs = [None for _ in range(self.c)]
+        self.g_prev = [None for _ in range(self.c)]
         self.a = [None for _ in range(self.c)]
         self.manager_returns = [None for _ in range(self.c)]
         self.worker_returns = [None for _ in range(self.c)]
@@ -100,6 +112,7 @@ class FeudalBatchProcessor(object):
 
         # extend with the actual values
         self.obs.extend(batch.obs)
+        self.g_prev.extend(batch.g_prev)
         self.a.extend(batch.a)
         self.manager_returns.extend(batch.manager_returns)
         self.worker_returns.extend(batch.worker_returns)
@@ -156,8 +169,8 @@ class FeudalBatchProcessor(object):
                 gsum += self.g[i]
 
             # add to the batch
-            feudal_batch.add(self.obs[t], self.a[t], self.manager_returns[t], 
-                self.worker_returns[t], s_diff, ri, gsum, self.features[t])
+            feudal_batch.add(self.obs[t], self.a[t], self.manager_returns[t],
+                self.worker_returns[t], s_diff, ri, gsum,self.g_prev[t], self.features[t])
 
         # in the terminal case, set reset flag
         if batch.terminal:
@@ -168,6 +181,7 @@ class FeudalBatchProcessor(object):
         else:
             twoc = 2 * self.c
             self.obs = self.obs[-twoc:]
+            self.g_prev = self.g_prev[-twoc:]
             self.a = self.a[-twoc:]
             self.manager_returns = self.manager_returns[-twoc:]
             self.worker_returns = self.worker_returns[-twoc:]

feudal_networks/policies/feudal_policy.py
@@ -44,7 +44,7 @@ class FeudalPolicy(policy.Policy):
 
     def _build_placeholders(self):
         # standard for all policies
-        self.obs = tf.placeholder(tf.float32, 
+        self.obs = tf.placeholder(tf.float32,
             shape=[None] + list(self.obs_space),
             name='obs')
         self.manager_r = tf.placeholder(tf.float32,
@@ -58,8 +58,8 @@ class FeudalPolicy(policy.Policy):
             name='action_mask')
 
         # specific to FeUdal
-        self.prev_g = tf.placeholder(tf.float32, 
-            shape=(None, None, self.g_dim),
+        self.prev_g = tf.placeholder(tf.float32,
+            shape=(None, self.config.c-1, self.g_dim),
             name='goals')
         self.ri = tf.placeholder(tf.float32,
             shape=(None,),
@@ -105,25 +105,25 @@ class FeudalPolicy(policy.Policy):
             if self.config.manager_rnn_type == 'dilated':
                 self.manager_state_in = [
                     tf.placeholder(
-                        shape=(1, self.config.manager_lstm_size), 
+                        shape=(1, self.config.manager_lstm_size),
                         dtype='float32',
                         name='manger_lstm_in1'),
                     tf.placeholder(
-                        shape=(1, self.config.manager_lstm_size), 
+                        shape=(1, self.config.manager_lstm_size),
                         dtype='float32',
                         name='manger_lstm_in2')
                 ]
                 lstm_output, self.manager_state_init, self.manager_state_out = DilatedLSTM(
-                    x, 
-                    self.config.manager_lstm_size, 
-                    self.manager_state_in, 
+                    x,
+                    self.config.manager_lstm_size,
+                    self.manager_state_in,
                     chunks=self.config.c
                 )
 
             elif self.config.manager_rnn_type == 'lstm':
                 self.manager_lstm = SingleStepLSTM(
-                    x, 
-                    self.config.manager_lstm_size, 
+                    x,
+                    self.config.manager_lstm_size,
                     step_size=tf.shape(self.obs)[:1]
                 )
                 self.manager_state_in = self.manager_lstm.state_in
@@ -133,21 +133,24 @@ class FeudalPolicy(policy.Policy):
             else:
                 raise ValueError('invalid config.rnn_type: {}'.format(
                     self.config.manager_rnn_type))
-            
+
             hidden_g_hat = tf.layers.dense(
                         inputs=lstm_output,
                         units=self.config.g_dim,
                         activation=tf.nn.elu,
                         name='hidden_g_hat'
             )
-            g_hat = linear(hidden_g_hat, self.config.g_dim, 'g_hat', 
+            g_hat = linear(hidden_g_hat, self.config.g_dim, 'g_hat',
                 initializer=normalized_columns_initializer(1.0)
             )
+            # g_hat = tf.cond(tf.random_uniform(()) < self.config.g_eps,
+            #                     lambda: tf.random_normal(tf.shape(g_hat)),
+            #                     lambda: g_hat)
 
             self.g = tf.nn.l2_normalize(g_hat, dim=1)
 
             if self.config.verbose:
-                self.g = tf.Print(self.g, [self.g, g_hat], 
+                self.g = tf.Print(self.g, [self.g, g_hat],
                     message='\nmanager g and g_hat: ', summarize=5)
 
             self.manager_vf = self._build_value(lstm_output)
@@ -169,25 +172,25 @@ class FeudalPolicy(policy.Policy):
             # Calculate w
             cut_g = tf.stop_gradient(self.g)
             cut_g = tf.expand_dims(cut_g, [1])
-            gstack = tf.concat([self.prev_g, cut_g], axis=1)
+            gstack = tf.concat([tf.zeros((tf.shape(cut_g)[0],self.config.c,self.g_dim)),self.prev_g, cut_g], axis=1)
 
-            self.last_c_g = gstack[:,1:]
-            gsum = tf.reduce_sum(gstack, axis=1)
+            self.last_c_g = gstack[:,-(self.config.c-1):]
+            self.g_sum=gsum = tf.reduce_sum(gstack, axis=1)
 
             if self.config.verbose:
-                gsum = tf.Print(gsum, [gsum], 
+                gsum = tf.Print(gsum, [gsum],
                     message='\nworker gsum: ', summarize=32)
 
             phi = tf.get_variable("phi", (self.g_dim, self.k),
                 initializer=normalized_columns_initializer(1.))
 
             if self.config.verbose:
-                phi = tf.Print(phi, [phi], 
+                phi = tf.Print(phi, [phi],
                     message='\nworker phi: ', summarize=5)
 
             w = tf.matmul(gsum, phi)
             tf.summary.scalar('model/w_magnitude', tf.reduce_mean(w))
-            
+
             # Calculate U
             self.worker_lstm = SingleStepLSTM(
                 tf.expand_dims(self.z, [0]),
@@ -199,7 +202,7 @@ class FeudalPolicy(policy.Policy):
             if self.config.worker_hint:
                 worker_hint = tf.stop_gradient(tf.contrib.layers.batch_norm(w))
                 lstm_output = tf.concat([lstm_output, worker_hint], axis=1)
-            
+
             self.worker_vf = self._build_value(lstm_output)
 
             flat_logits_hidden = tf.layers.dense(
@@ -220,42 +223,44 @@ class FeudalPolicy(policy.Policy):
                 name='flat_logits'
             )
 
-            self.U = tf.reshape(flat_logits, 
+            self.U = tf.reshape(flat_logits,
                 shape=[-1, num_acts, self.k],
                 name='U')
 
             if self.config.verbose:
-                self.U = tf.Print(self.U, [self.U], 
+                self.U = tf.Print(self.U, [self.U],
                     message='\nworker U: ', summarize=num_acts * self.k)
 
-            tf.summary.image('model/U', 
+            tf.summary.image('model/U',
                 tf.reshape(self.U, (-1, num_acts, self.k, 1)), max_outputs=1)
-            tf.summary.image('model/w', 
+            tf.summary.image('model/w',
                 tf.reshape(w, (-1, 1, self.k, 1)), max_outputs=1)
 
             # expand dims for combining with U
             w = tf.expand_dims(w, [2])
 
             if self.config.verbose:
-                w = tf.Print(w, [w], 
+                w = tf.Print(w, [w],
                     message='\nworker w: ', summarize=self.k)
 
             # calculate policy and sample
             logits = tf.reshape(tf.matmul(self.U, w),[-1, num_acts])
 
-            tf.summary.image('model/logits', 
+            tf.summary.image('model/logits',
                 tf.reshape(logits, (-1, 1, num_acts, 1)), max_outputs=1)
 
             self.pi = tf.nn.softmax(logits)
             self.log_pi = tf.nn.log_softmax(logits)
 
             if self.config.verbose:
-                self.pi = tf.Print(self.pi, [logits, self.pi, self.log_pi], 
+                self.pi = tf.Print(self.pi, [logits, self.pi, self.log_pi],
                     message='\nworker logits, pi, log_pi: ', summarize=15)
 
             self.sample = policy_utils.categorical_sample(
                 tf.reshape(logits,[-1, num_acts]), num_acts)[0, :]
 
+            if self.config.david_debug:
+                self.sample = tf.Print(self.sample,[self.sample,self.pi[0,:],self.g[0,:]],message='Sampled',summarize=6)
             # add worker c, h to state in and out
             self.state_in.extend([
                 self.worker_lstm.state_in[0],
@@ -266,6 +271,7 @@ class FeudalPolicy(policy.Policy):
                 self.worker_lstm.state_out[1],
             ])
 
+
     def _build_value(self,input):
         with tf.variable_scope('VF'):
             hidden = tf.layers.dense(
@@ -274,7 +280,7 @@ class FeudalPolicy(policy.Policy):
                         activation=tf.nn.elu,
                         name='hidden'
             )
-            vf = linear(hidden, 1, 'value', 
+            vf = linear(hidden, 1, 'value',
                 initializer=normalized_columns_initializer(1.0)
             )
             return vf
@@ -284,9 +290,9 @@ class FeudalPolicy(policy.Policy):
         # manager policy loss
         if self.config.similarity_metric == 'cosine':
             dot = tf.reduce_sum(tf.multiply(self.s_diff, self.g), axis=1)
-            # add epislon in before the norm, otherwise can get nans when 
-            # g is all zeros and try to backprop the norm 
-            # can also prevent by stopping gradient, but this actually 
+            # add epislon in before the norm, otherwise can get nans when
+            # g is all zeros and try to backprop the norm
+            # can also prevent by stopping gradient, but this actually
             # should be included as part of the backprop
             gcut = self.g + self.config.eps
             # also have to epsilon here as well in case s_diff is 0
@@ -295,7 +301,7 @@ class FeudalPolicy(policy.Policy):
             similarity = dcos
 
             if self.config.verbose:
-                dcos = tf.Print(dcos, [dcos, dot, mag], 
+                dcos = tf.Print(dcos, [dcos, dot, mag],
                     message='\nfeudal dcos, dot, magnitude: ', summarize=15)
             tf.summary.scalar("model/dcos", tf.reduce_mean(dcos))
             tf.summary.scalar("model/dcos_magnitude", tf.reduce_mean(mag))
@@ -303,9 +309,9 @@ class FeudalPolicy(policy.Policy):
         elif self.config.similarity_metric == 'gaussian':
             diff = self.s_diff - self.g
             similarity = tf.exp(-tf.reduce_sum(tf.multiply(diff, diff), axis=-1))
-            
+
             if self.config.verbose:
-                similarity = tf.Print(similarity, [similarity], 
+                similarity = tf.Print(similarity, [similarity],
                     message='\ngaussian pdf of g and s_diff: ', summarize=15)
             tf.summary.scalar("model/gaussian_pdf", tf.reduce_mean(similarity))
 
@@ -315,8 +321,8 @@ class FeudalPolicy(policy.Policy):
 
         cutoff_vf_manager = tf.reshape(tf.stop_gradient(self.manager_vf),[-1])
         if self.config.verbose:
-            cutoff_vf_manager = tf.Print(cutoff_vf_manager, 
-                [self.manager_r, cutoff_vf_manager], 
+            cutoff_vf_manager = tf.Print(cutoff_vf_manager,
+                [self.manager_r, cutoff_vf_manager],
                 message='\nfeudal loss return, manager vf: ', summarize=10)
 
         manager_loss = -tf.reduce_sum((self.manager_r - cutoff_vf_manager) * similarity)
@@ -328,13 +334,15 @@ class FeudalPolicy(policy.Policy):
         # worker policy loss
         cutoff_vf_worker = tf.reshape(tf.stop_gradient(self.worker_vf), [-1])
         log_p = tf.reduce_sum(self.log_pi * self.ac, axis=1)
+        if self.config.david_debug:
+            log_p = tf.Print(log_p,[self.ac[0,:],self.pi[0,:],self.g[0,:]],message='loss',summarize=6)
 
         if self.config.verbose:
-            log_p = tf.Print(log_p, [self.ac], 
+            log_p = tf.Print(log_p, [self.ac],
                 message='\naction mask: ', summarize=10)
 
         alpha = tf.train.polynomial_decay(
-            self.config.alpha_start, 
+            self.config.alpha_start,
             self.global_step,
             end_learning_rate=self.config.alpha_end,
             decay_steps=self.config.alpha_steps,
@@ -350,7 +358,7 @@ class FeudalPolicy(policy.Policy):
 
         entropy = -tf.reduce_sum(self.pi * self.log_pi)
         beta = tf.train.polynomial_decay(
-            self.config.beta_start, 
+            self.config.beta_start,
             self.global_step,
             end_learning_rate=self.config.beta_end,
             decay_steps=self.config.decay_steps,
@@ -359,14 +367,14 @@ class FeudalPolicy(policy.Policy):
 
         reg_loss = tf.contrib.layers.apply_regularization(
             tf.contrib.layers.l2_regularizer(self.config.l2_reg),
-            tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 
+            tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,
                 tf.get_variable_scope().name))
 
         # loss comprised of the individual losses
         self.loss = (
-            worker_loss 
-            + manager_loss 
-            + worker_vf_loss 
+            worker_loss
+            + manager_loss
+            + worker_vf_loss
             + manager_vf_loss * self.config.manager_value_loss_weight
             - entropy * beta
             + reg_loss
@@ -376,23 +384,23 @@ class FeudalPolicy(policy.Policy):
         tf.summary.scalar("model/manager_pi_loss", manager_loss / bs)
         tf.summary.scalar("model/worker_pi_loss", worker_loss / bs)
         tf.summary.scalar("model/value_mean", tf.reduce_mean(self.manager_vf))
-        tf.summary.scalar("model/manager_value_loss_scaled", 
+        tf.summary.scalar("model/manager_value_loss_scaled",
             manager_vf_loss / bs * .5 *  self.config.manager_value_loss_weight)
         tf.summary.scalar("model/worker_value_loss_scaled", worker_vf_loss / bs * .5)
         tf.summary.scalar("model/entropy", entropy / bs)
         tf.summary.scalar("model/mean_log_pi_of_action", tf.reduce_mean(log_p))
         tf.summary.scalar("model/entropy_loss_scaled", -entropy / bs * beta)
         tf.summary.scalar("model/l2_reg_loss_scaled", reg_loss)
-        tf.summary.scalar("model/var_global_norm", 
+        tf.summary.scalar("model/var_global_norm",
             tf.global_norm(tf.get_collection(
-                tf.GraphKeys.TRAINABLE_VARIABLES, 
+                tf.GraphKeys.TRAINABLE_VARIABLES,
                 tf.get_variable_scope().name)))
         tf.summary.scalar("model/beta", beta)
         tf.summary.scalar("model/alpha", alpha)
         tf.summary.image("model/obs", self.obs, max_outputs=1)
 
         # additional summaries
-        tf.summary.image("model/summed_obs", 
+        tf.summary.image("model/summed_obs",
             tf.reduce_mean(self.obs, axis=0, keep_dims=True))
         if np.sqrt(self.config.g_dim) == int(np.sqrt(self.config.g_dim)):
             side_length = int(np.sqrt(self.config.g_dim))
@@ -410,20 +418,20 @@ class FeudalPolicy(policy.Policy):
         self.summary_op = tf.summary.merge_all()
 
     def get_initial_features(self):
-        return np.zeros((1, 1, self.g_dim), np.float32), self.worker_lstm.state_init + self.manager_state_init
+        return np.zeros((1, self.config.c-1, self.g_dim), np.float32), self.worker_lstm.state_init + self.manager_state_init
 
-    def act(self, ob, g, cw, hw, cm, hm):
+    def act(self, ob, g, cm, hm, cw, hw):
         sess = tf.get_default_session()
         return sess.run([self.sample, self.manager_vf, self.g, self.s, self.last_c_g] + self.state_out,
-                        {self.obs: [ob], self.state_in[0]: cw, self.state_in[1]: hw,\
-                         self.state_in[2]: cm, self.state_in[3]: hm,\
+                        {self.obs: [ob], self.state_in[0]: cm, self.state_in[1]: hm,\
+                         self.state_in[2]: cw, self.state_in[3]: hw,\
                          self.prev_g: g})
 
-    def value(self, ob, g, cw, hw, cm, hm):
+    def value(self, ob, g, cm, hm, cw, hw):
         sess = tf.get_default_session()
         manager_vf, worker_vf = sess.run([self.manager_vf, self.worker_vf],
-                        {self.obs: [ob], self.state_in[0]: cw, self.state_in[1]: hw,\
-                         self.state_in[2]: cm, self.state_in[3]: hm,\
+                        {self.obs: [ob], self.state_in[0]: cm, self.state_in[1]: hm,\
+                         self.state_in[2]: cw, self.state_in[3]: hw,\
                          self.prev_g: g})
         return manager_vf[0], worker_vf[0]
 