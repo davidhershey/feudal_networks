015715a4f581ba9e9a2a92ee72548a0cc63d613a
David Hershey <dshersh@stanford.edu>
2017-06-08 12:15:46 -0700

    Updated default config

--- scripts/training/configs/dlstm_config.py
+++ scripts/training/configs/dlstm_config.py
@@ -2,7 +2,7 @@ class Config(object):
     size = 256
     n_percept_hidden_layer = 4
     n_percept_filters = 32
-    beta_start = .01
+    beta_start = .001
     beta_end = .001
     decay_steps = 50000000
     summary_steps = 10
