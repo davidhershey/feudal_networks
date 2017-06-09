ad7e1da72331cf0f0e0f8e31bc49f30e4c9a43fc
David Hershey <dmakian@gmail.com>
2017-06-09 19:44:59 +0000

    Added dlstm maze config

---
+++ scripts/training/configs/dlstm_maze_config.py
@@ -0,0 +1,12 @@
+class Config(object):
+    size = 256
+    num_local_steps = 80
+    n_percept_hidden_layer = 4
+    n_percept_filters = 32
+    beta_start = .001
+    beta_end = .001
+    decay_steps = 50000000
+    summary_steps = 10
+    gamma = .99
+    chunks = 4
+    lr = 1e-3
